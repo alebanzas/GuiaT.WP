c0d71ab7e11fe4b5da7649c92ce57bdcf271dedf
abanzas <abanzas@localhost>
2013-03-06 19:32:59 -0300

    avanzo colectivos

--- GuiaTBAWP/Colectivos.xaml.cs
+++ GuiaTBAWP/Colectivos.xaml.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using GuiaTBAWP.BusData;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
@@ -15,6 +16,55 @@ namespace GuiaTBAWP
         public Colectivos()
         {
             InitializeComponent();
+
+            LoadBusesIUrb();
+            LoadBusesMuni();
+            LoadBusesProv();
+        }
+
+        private void LoadBusesIUrb()
+        {
+            var buss = new List<Bus>();
+
+
+
+
+            var byCategory = from Bus in buss
+                             group Bus by Bus.Category into c
+                             //orderby c.Key
+                             select new PublicGrouping<string, Bus>(c);
+
+            busesIUrb.ItemsSource = byCategory;
+        }
+
+        private void LoadBusesMuni()
+        {
+            var buss = new List<Bus>();
+
+
+
+
+            var byCategory = from Bus in buss
+                             group Bus by Bus.Category into c
+                             //orderby c.Key
+                             select new PublicGrouping<string, Bus>(c);
+
+            busesMuni.ItemsSource = byCategory;
+        }
+
+        private void LoadBusesProv()
+        {
+            var buss = new List<Bus>();
+
+
+
+
+            var byCategory = from Bus in buss
+                             group Bus by Bus.Category into c
+                             //orderby c.Key
+                             select new PublicGrouping<string, Bus>(c);
+
+            busesProv.ItemsSource = byCategory;
         }
     }
 }

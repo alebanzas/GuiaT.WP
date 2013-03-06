55c6ee1879e201924114cdf85a3003436861ba29
Alejandro Banzas <alebanzas@outlook.com>
2013-03-06 19:32:59 -0300

    avanzo colectivos

---
+++ GuiaTBAWP/BusData/Bus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GuiaTBAWP.BusData
+{
+    public class Bus
+    {
+        private Bus() { }
+
+        public Bus(string category)
+        {
+            this.Category = category;
+        }
+
+        public static readonly string[] Categories = { "Action", "Romance", "Thrillers", "Comedy", "Documentaries", "Drama" };
+        
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; private set; }
+    }
+}

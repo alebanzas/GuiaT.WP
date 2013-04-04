b1b7ccb656c7246ea05f1929e63ebcc2cf098094
Alejandro Banzas <alebanzas@outlook.com>
2013-04-04 00:18:21 -0300

    trenes estado

---
+++ data/Colectivos/WebScraping/WebScraping/Bicicletas.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+
+namespace WebScraping
+{
+    public partial class Bicicletas : System.Web.UI.Page
+    {
+        public string Resultado { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Show();
+
+        }
+        
+        protected void Show()
+        {
+            string result = string.Empty;
+
+            HtmlNode html = new Scraper().GetNodes(new Uri("http://www.bicicletapublica.com.ar/mapa.aspx"));
+
+            var cssSelect = html.CssSelect("script");
+            var script = cssSelect.Skip(1).FirstOrDefault().InnerText;
+            
+            foreach (var posta in script.Split(new[] { "new GLatLng(" }, StringSplitOptions.RemoveEmptyEntries).Skip(2))
+            {
+                var a = posta.Split(new[] {"openInfoWindowHtml('"}, StringSplitOptions.RemoveEmptyEntries);
+
+                //-34.592308,-58.37501
+                string arg0 = a[0].Split(new[] { ")," }, StringSplitOptions.RemoveEmptyEntries)[0];
+
+                var lat = double.Parse(arg0.Split(',')[0].Trim(), NumberStyles.Any, CultureInfo.InvariantCulture);
+                var lon = double.Parse(arg0.Split(',')[1].Trim(), NumberStyles.Any, CultureInfo.InvariantCulture);
+                
+                //<div style="height:100px;"><span class="style1">RETIRO
+                //<br>Cerrado. Horario de atención: Lun a Vie de 8 a 20. Sáb 9 a 15.</span>
+                //<br><span class="style2">Cant. Bicicletas disponibles: 8</span><br></div>
+                string arg1 = a[1].Split(new[] { "'," }, StringSplitOptions.RemoveEmptyEntries)[0];
+
+                var arg2 = arg1.Split(new[] {"<br>"}, StringSplitOptions.RemoveEmptyEntries);
+
+                var nombre = arg2[0].Split('>')[2].Trim();
+
+                var estado = arg2[1].Split('.')[0].Trim();
+
+                var horario = arg2[1].Split(':')[1].Split('<')[0].Trim();
+
+                var cantidad = int.Parse(arg2[2].Split(':')[1].Split('<')[0].Trim());
+                
+                var estacion = new BicicletaEstacion();
+                estacion.Latitud = lat;
+                estacion.Longitud = lon;
+                estacion.Nombre = nombre;
+                estacion.Estado = estado;
+                estacion.Horario = horario;
+                estacion.Cantidad = cantidad;
+
+
+                result += string.Format("{0} <br><br> {1} <br><br> {2} <br>", arg0, arg1, string.Empty);
+            }
+            
+            Resultado = result;
+        }
+
+    }
+
+    public class BicicletaEstacion
+    {
+        public double Latitud { get; set; }
+
+        public double Longitud { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Horario { get; set; }
+
+        public int Cantidad { get; set; }
+    }
+}
--- data/Colectivos/WebScraping/WebScraping/Default.aspx.cs
+++ data/Colectivos/WebScraping/WebScraping/Default.aspx.cs
@@ -22,65 +22,73 @@ namespace WebScraping
         {
             string result = string.Empty;
 
-            HtmlNode html = new Scraper().GetNodes(new Uri("http://www.bicicletapublica.com.ar/mapa.aspx"));
+            HtmlNode html = new Scraper().GetNodes(new Uri("http://servicios.lanacion.com.ar/transito/"));
 
-            var cssSelect = html.CssSelect("script");
-            var script = cssSelect.Skip(1).FirstOrDefault().InnerText;
+            var cssSelect = html.CssSelect("section.trenes");
+            var script = cssSelect.CssSelect("nav ul li");
             
-            foreach (var posta in script.Split(new[] { "new GLatLng(" }, StringSplitOptions.RemoveEmptyEntries).Skip(2))
+            List<LineaTrenModel> lineas = new List<LineaTrenModel>();
+
+            foreach (var linea in script)
             {
-                var a = posta.Split(new[] {"openInfoWindowHtml('"}, StringSplitOptions.RemoveEmptyEntries);
+                var classNumber = linea.GetAttributeValue("class");
 
-                //-34.592308,-58.37501
-                string arg0 = a[0].Split(new[] { ")," }, StringSplitOptions.RemoveEmptyEntries)[0];
+                var aux = cssSelect.CssSelect("div.ramales ul." + classNumber);
 
-                var lat = double.Parse(arg0.Split(',')[0].Trim(), NumberStyles.Any, CultureInfo.InvariantCulture);
-                var lon = double.Parse(arg0.Split(',')[1].Trim(), NumberStyles.Any, CultureInfo.InvariantCulture);
+                var ramales = aux.CssSelect("li");
+
+                List<RamalTrenModel> r = new List<RamalTrenModel>();
+                foreach (var ramal in ramales)
+                {
+                    /*
+                     <li>
+                     * Retiro-Villa Rosa<br>
+                     * <a href="#" class="normal" alt="Normal" title="Normal">Normal</a>
+                     * <span>
+                     * <b class="color">Retiro-Villa Rosa</b>
+                     * <br><b class="normal">Normal</b>
+                     * <div class="separador"></div></span></li>
+                     */
+
+                    var ra = new RamalTrenModel();
+                    ra.Nombre = ramal.InnerHtml.Split(new[] { "<br>" }, StringSplitOptions.RemoveEmptyEntries)[0];
+                    ra.Estado = ramal.CssSelect("a").FirstOrDefault().InnerText;
+                    ra.MasInfo = "";
+
+                    r.Add(ra);
+                }
+
                 
-                //<div style="height:100px;"><span class="style1">RETIRO
-                //<br>Cerrado. Horario de atención: Lun a Vie de 8 a 20. Sáb 9 a 15.</span>
-                //<br><span class="style2">Cant. Bicicletas disponibles: 8</span><br></div>
-                string arg1 = a[1].Split(new[] { "'," }, StringSplitOptions.RemoveEmptyEntries)[0];
+                var l = new LineaTrenModel();
+                l.Nombre = linea.CssSelect("a").FirstOrDefault().InnerText;
+                l.Ramales = r;
 
-                var arg2 = arg1.Split(new[] {"<br>"}, StringSplitOptions.RemoveEmptyEntries);
-
-                var nombre = arg2[0].Split('>')[2].Trim();
-
-                var estado = arg2[1].Split('.')[0].Trim();
-
-                var horario = arg2[1].Split(':')[1].Split('<')[0].Trim();
-
-                var cantidad = int.Parse(arg2[2].Split(':')[1].Split('<')[0].Trim());
-                
-                var estacion = new BicicletaEstacion();
-                estacion.Latitud = lat;
-                estacion.Longitud = lon;
-                estacion.Nombre = nombre;
-                estacion.Estado = estado;
-                estacion.Horario = horario;
-                estacion.Cantidad = cantidad;
-
-
-                result += string.Format("{0} <br><br> {1} <br><br> {2} <br>", arg0, arg1, string.Empty);
+                lineas.Add(l);
             }
             
             Resultado = result;
         }
 
     }
-
-    public class BicicletaEstacion
+    
+    public class LineaTrenModel
     {
-        public double Latitud { get; set; }
+        public string Nombre { get; set; }
 
-        public double Longitud { get; set; }
+        /// <summary>
+        /// TODO: el peor de los estados de sus ramales
+        /// </summary>
+        public string Estado { get; set; }
 
+        public List<RamalTrenModel> Ramales { get; set; }
+    }
+
+    public class RamalTrenModel
+    {
         public string Nombre { get; set; }
 
         public string Estado { get; set; }
 
-        public string Horario { get; set; }
-
-        public int Cantidad { get; set; }
+        public string MasInfo { get; set; }
     }
 }

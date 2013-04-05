77a0c2cc310db7cb6c72882dbbd5782f881f7049
abanzas <abanzas@localhost>
2013-04-05 19:08:38 -0300

    view models

---
+++ GuiaTBAWP/Models/LineaTrenModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace GuiaTBAWP.Models
+{
+    public class LineaTrenModel
+    {
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// TODO: el peor de los estados de sus ramales
+        /// </summary>
+        public string Estado { get; set; }
+
+        public List<RamalTrenModel> Ramales { get; set; }
+    }
+}
---
+++ GuiaTBAWP/Models/TrenesStatusModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuiaTBAWP.Models
+{
+    public class TrenesStatusModel
+    {
+        public TrenesStatusModel()
+        {
+            Lineas = new List<LineaTrenModel>();
+        }
+
+        public IList<LineaTrenModel> Lineas { get; set; }
+
+        public DateTime Actualizacion { get; set; }
+
+        public string ActualizacionStr
+        {
+            get { return string.Format("{0} {1}", Actualizacion.ToLongDateString(), Actualizacion.ToLongTimeString()); }
+        }
+    }
+}

4e9db1f7e1f2db48700e490e1429e73c7de59d29
Alejandro Banzas <alebanzas@outlook.com>
2013-04-07 01:06:00 -0300

    traigo datos del tren

--- GuiaTBAWP/Views/Trenes/BelgranoNorte.xaml.cs
+++ GuiaTBAWP/Views/Trenes/BelgranoNorte.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Phone.Controls;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using GuiaTBAWP.Models;
+using GuiaTBAWP.ViewModels;
+using Microsoft.Phone.Controls;
 
 namespace GuiaTBAWP.Views.Trenes
 {
@@ -7,6 +12,35 @@ namespace GuiaTBAWP.Views.Trenes
         public BelgranoNorte()
         {
             InitializeComponent();
+
+            DataContext = ViewModel;
+            Loaded += Page_Loaded;
+        }
+
+        private static TrenLineaItemViewModel _viewModel = new TrenLineaItemViewModel();
+
+        /// <summary>
+        /// A static ViewModel used by the views to bind against.
+        /// </summary>
+        /// <returns>The MainViewModel object.</returns>
+        public static TrenLineaItemViewModel ViewModel
+        {
+            get
+            {
+                // Delay creation of the view model until necessary
+                return _viewModel ?? (_viewModel = new TrenLineaItemViewModel());
+            }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = TrenesRamalEstadoDC.Current.ByLinea("belgrano norte");
+
+            foreach (var estadoTable in query.ToList())
+            {
+                ViewModel.AddRamal(estadoTable.ConvertToTrenRamalItemViewModel());
+            }
+            ViewModel.Actualizacion = (App.Current as App).UltimaActualizacionTrenes;
         }
     }
 }

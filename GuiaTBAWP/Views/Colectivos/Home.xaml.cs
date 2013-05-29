1ccecaf2ce16d3d3356eb2ce39e1ae9878fed14a
abanzas <abanzas@localhost>
2013-05-28 22:25:30 -0300

    empiezo colectivos cercanos

---
+++ GuiaTBAWP/Views/Colectivos/Home.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace GuiaTBAWP.Views.Colectivos
+{
+    public partial class Home : PhoneApplicationPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Colectivos(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/Views/Colectivos/Colectivos.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click_ColectivosCercanos(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/Views/Colectivos/Cercanos.xaml", UriKind.Relative));
+        }
+    }
+}

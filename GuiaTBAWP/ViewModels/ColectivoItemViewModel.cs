d5abe9b468bab613b660b2c26c7e656d9c09dc21
abanzas <abanzas@localhost>
2013-07-07 20:38:56 -0300

    arranco pagina de detalle de linea de colectivo

--- GuiaTBAWP/ViewModels/ColectivoItemViewModel.cs
+++ GuiaTBAWP/ViewModels/ColectivoItemViewModel.cs
@@ -2,6 +2,8 @@
 {
     public class ColectivoItemViewModel
     {
+        public string Id { get; set; }
+
         public string Nombre { get; set; }
 
         public string Detalles { get; set; }

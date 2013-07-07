63131d50cf9a8d3d9b6b57b18fe7f2f7edc6c32a
Alejandro Banzas <alebanzas@outlook.com>
2013-07-07 20:37:58 -0300

    pagina de error con reporte

--- GuiaTBAWP/GeneralError.xaml.cs
+++ GuiaTBAWP/GeneralError.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 
@@ -17,6 +18,24 @@ namespace GuiaTBAWP
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             ErrorText.Text = Exception.ToString();
+        }
+
+        private void GoToHome_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.NavigationService.Navigate(new Uri("/Home.xaml", UriKind.Relative));
+            }
+            catch (Exception)
+            {
+                Dispatcher.BeginInvoke(() => MessageBox.Show("Por favor, vuelva a iniciar la aplicación manualmente."));
+            }
+        }
+
+        private void SendErrorReport_OnClick(object sender, RoutedEventArgs e)
+        {
+            DvError.Visibility = Visibility.Collapsed;
+            DvThanks.Visibility = Visibility.Visible;
         }
     }
 }

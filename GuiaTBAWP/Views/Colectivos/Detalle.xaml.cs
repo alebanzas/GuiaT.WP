98ea2986d5c1c63cd5f8fe63c955f988b72cd8fa
Alejandro Banzas <alebanzas@outlook.com>
2013-07-07 20:38:56 -0300

    arranco pagina de detalle de linea de colectivo

---
+++ GuiaTBAWP/Views/Colectivos/Detalle.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using GuiaTBAWP.BusData;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace GuiaTBAWP.Views.Colectivos
+{
+    public partial class Detalle : PhoneApplicationPage
+    {
+        public Detalle()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            LoadData();
+            base.OnNavigatedTo(e);
+        }
+
+        private void LoadData()
+        {
+            var linea = Uri.EscapeUriString(NavigationContext.QueryString["id"]);
+            var colectivoRecorrido = DataColectivos.AllByCode(linea);
+
+            GeneralPanorama.Title = "Línea " + linea;
+            /*
+             <controls:PanoramaItem Header="recorrido" HeaderTemplate="{StaticResource SmallHomePanoramaTitle}">
+                <ScrollViewer Margin="12,0,0,0">
+                    <TextBlock x:Name="TxtRecorrido"></TextBlock>
+                </ScrollViewer>
+            </controls:PanoramaItem>
+             */
+
+            foreach (var colectivo in colectivoRecorrido)
+            {
+                var text = new TextBlock();
+                text.Text = colectivo.Value;
+                text.TextWrapping = TextWrapping.Wrap;
+
+                var scroll = new ScrollViewer();
+                scroll.Margin = new Thickness(12,0,0,40);
+                scroll.Content = text;
+
+                var item = new PanoramaItem();
+                item.Header = colectivo.Key;
+                item.Content = scroll;
+
+                GeneralPanorama.Items.Add(item);
+            }
+
+        }
+    }
+}

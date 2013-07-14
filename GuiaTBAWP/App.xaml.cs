7cea187a10270a17820baead4dbbe50cb49d547d
Alejandro Banzas <alebanzas@outlook.com>
2013-07-14 14:50:34 -0300

    guardo nueva configuracion cuando se cierra o desactiva la app

--- GuiaTBAWP/App.xaml.cs
+++ GuiaTBAWP/App.xaml.cs
@@ -73,12 +73,14 @@ namespace GuiaTBAWP
         // This code will not execute when the application is closing
         private void Application_Deactivated(object sender, DeactivatedEventArgs e)
         {
+            Config.Set(Configuration);
         }
 
         // Code to execute when the application is closing (eg, user hit Back)
         // This code will not execute when the application is deactivated
         private void Application_Closing(object sender, ClosingEventArgs e)
         {
+            Config.Set(Configuration);
         }
 
         // Code to execute if a navigation fails

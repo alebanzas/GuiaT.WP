d2ab58a2e0bb8af2bf3053d621b6666903568cee
Alejandro Banzas <alebanzas@outlook.com>
2013-07-21 18:59:00 -0300

    empiezo refactor de sube

--- GuiaTBAWP/Views/SUBE/DondeCargar.xaml.cs
+++ GuiaTBAWP/Views/SUBE/DondeCargar.xaml.cs
@@ -1,117 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Device.Location;
 using System.Globalization;
-using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Runtime.Serialization.Json;
 using System.Windows;
 using System.Windows.Input;
-using GuiaTBAWP.Bing.Geocode;
 using GuiaTBAWP.Extensions;
 using GuiaTBAWP.Models;
 using GuiaTBAWP.ViewModels;
-using Microsoft.Phone.Controls;
 using Microsoft.Phone.Controls.Maps;
-using Microsoft.Phone.Net.NetworkInformation;
 using Microsoft.Phone.Shell;
 
 namespace GuiaTBAWP.Views.SUBE
 {
-    public partial class DondeCargar : PhoneApplicationPage
+    public partial class DondeCargar
     {
         private static MainViewModel _viewModel;
-        /// <summary>
-        /// A static ViewModel used by the views to bind against.
-        /// </summary>
-        /// <returns>The MainViewModel object.</returns>
         public static MainViewModel ViewModel
         {
-            get
-            {
-                // Delay creation of the view model until necessary
-                return _viewModel ?? (_viewModel = new MainViewModel());
-            }
+            get { return _viewModel ?? (_viewModel = new MainViewModel()); }
         }
-        
-        #region TODO: mover
-
-
-        public static void RefreshAllPushpins()
-        {
-            foreach (var recarga in ViewModel.PuntosRecarga)
-            {
-                CreateNewRecargaPushpin(recarga);
-            }
-        }
-
-        private void CreateNewPushpin(Point point)
-        {
-            // Translate the map viewport touch point to a geo coordinate.
-            GeoCoordinate location;
-            Mapa.TryViewportPointToLocation(point, out location);
-            CreateNewPushpin(location);
-        }
-
-        public static void CreateNewPushpin(GeoCoordinate location)
-        {
-            var pushpin = ViewModel.DefaultPushPin.Clone(location);
-            CreateNewPushpin(pushpin);
-        }
-        
-        public static void CreateNewRecargaPushpin(ItemViewModel itemViewModel)
-        {
-            var pushpin = ViewModel.RecargaPushPin.Clone(itemViewModel.Punto);
-            pushpin.Title = itemViewModel.Titulo;
-            pushpin.Index = itemViewModel.Index;
-            CreateNewPushpin(pushpin);
-        }
-
-        private static void CreateNewPushpin(PushpinModel pushpin)
-        {
-            ViewModel.Pushpins.Add(pushpin);
-        }
-
-/*
-        private void CreateNewPushpin(object selectedItem, Point point)
-        {
-            // Use the geo coordinate calculated to add a new pushpin,
-            // based on the selected pushpin prototype,
-            var pushpinPrototype = selectedItem as PushpinModel;
-            if (pushpinPrototype == null) return;
-
-            CreateNewPushpin(point);
-        }
-*/
-
-        #endregion
 
         private HttpWebRequest _httpReq;
 
         public DondeCargar()
         {
             InitializeComponent();
-
-            ResetUI();
-
-            Unloaded += DondeCargar_Unloaded;
-
+            
             Loaded += (s, e) =>
                 {
                     DataContext = ViewModel;
                     
-                    if (ViewModel.IsPuntosRecargaLoaded) return;
-
-                    RecargaLoading.Visibility = Visibility.Visible;
-                    
                     GetDondeCargar();
-                    //SetProgressBar("Buscando posición...");
-                    var applicationBarIconButton = ApplicationBar.Buttons[0] as ApplicationBarIconButton;
-                    if (applicationBarIconButton != null)
-                        applicationBarIconButton.IsEnabled = false;
                 };
+            Unloaded += DondeCargar_Unloaded;
         }
 
         private void DondeCargar_Unloaded(object sender, RoutedEventArgs e)
@@ -122,126 +47,85 @@ namespace GuiaTBAWP.Views.SUBE
         
         void GetDondeCargar()
         {
-            var currentLocation = PositionService.GetCurrentLocation();
-            var location = new GeocodeLocation
+            ResetUI();
+            if (!NetworkInterface.GetIsNetworkAvailable())
             {
-                Latitude = currentLocation.Location.Latitude,
-                Longitude = currentLocation.Location.Longitude
-            };
-
-            if (Math.Abs(location.Latitude - ViewModel.CurrentLocation.Latitude) < App.Configuration.MinDiffGeography && Math.Abs(location.Longitude - ViewModel.CurrentLocation.Longitude) < App.Configuration.MinDiffGeography)
-            {
+                ConnectionError.Visibility = Visibility.Visible;
+                Dispatcher.BeginInvoke(() => MessageBox.Show("Ha habido un error intentando acceder a los nuevos datos o no hay conexiones de red disponibles.\nPor favor asegúrese de contar con acceso de red y vuelva a intentarlo."));
                 return;
             }
 
-            ViewModel.CurrentLocation = currentLocation.Location;
+            GeoPosition<GeoCoordinate> currentLocation = PositionService.GetCurrentLocation();
 
-            SetLocation(location);
-            CreateNewPushpin(location);
-            ProgressBar.Hide();
+            if (!App.Configuration.IsLocationEnabled)
+            {
+                Dispatcher.BeginInvoke(() => MessageBox.Show("Para buscar donde cargar SUBE, por favor, active la función de localización en la configuración de la aplicación."));
+                return;
+            }
 
-            GetMasCercanos();
-        }
-
-        private int _pendingRequests;
-        private void GetMasCercanos()
-        {
-            ProgressBar.Show("Buscando más cercano...");
+            ProgressBar.Show("Buscando más cercanos...");
+            SetApplicationBarEnabled(false);
+            CancelarRequest();
 
             var param = new Dictionary<string, object>
                 {
-                    {"lat", ViewModel.CurrentLocation.Latitude.ToString(CultureInfo.InvariantCulture).Replace(",", ".")},
-                    {"lon", ViewModel.CurrentLocation.Longitude.ToString(CultureInfo.InvariantCulture).Replace(",", ".")},
+                    {"lat", currentLocation.Location.Latitude.ToString(CultureInfo.InvariantCulture).Replace(",", ".")},
+                    {"lon", currentLocation.Location.Longitude.ToString(CultureInfo.InvariantCulture).Replace(",", ".")},
                     {"cant", 10},
                 };
 
             _httpReq = (HttpWebRequest)WebRequest.Create("/sube/recarganear".ToApiCallUri(param));
             _httpReq.Method = "GET";
             _httpReq.BeginGetResponse(HTTPWebRequestCallBack, _httpReq);
-            _pendingRequests++;
         }
 
         private void HTTPWebRequestCallBack(IAsyncResult result)
         {
-            try
-            {
-                var httpRequest = (HttpWebRequest)result.AsyncState;
-                var response = httpRequest.EndGetResponse(result);
-                var stream = response.GetResponseStream();
+            var httpRequest = (HttpWebRequest)result.AsyncState;
+            var response = httpRequest.EndGetResponse(result);
+            var stream = response.GetResponseStream();
 
-                var serializer = new DataContractJsonSerializer(typeof(List<SUBEPuntoModel>));
-                var o = (List<SUBEPuntoModel>)serializer.ReadObject(stream);
+            var serializer = new DataContractJsonSerializer(typeof(List<SUBEPuntoModel>));
+            var o = (List<SUBEPuntoModel>)serializer.ReadObject(stream);
 
-                Dispatcher.BeginInvoke(new DelegateUpdateWebBrowser(UpdateWebBrowser), o);
-            }
-            catch (Exception ex)
-            {
-                Dispatcher.BeginInvoke(() =>
-                    {
-                        RecargaLoading.Visibility = Visibility.Collapsed;
-                        RecargaError.Visibility = Visibility.Visible;
-                        FinishRequest();
-                    });
-                //this.Dispatcher.BeginInvoke(() => MessageBox.Show("Error... " + ex.Message));
-            }
+            Dispatcher.BeginInvoke(new DelegateUpdateWebBrowser(UpdateWebBrowser), o);
         }
 
         delegate void DelegateUpdateWebBrowser(List<SUBEPuntoModel> local);
         private void UpdateWebBrowser(List<SUBEPuntoModel> l)
         {
-            var model = new Collection<ItemViewModel>();
-            var i = 1;
-            foreach (var puntoModel in l)
-            {
-                var punto = new GeoCoordinate(puntoModel.Latitud, puntoModel.Longitud);
-                var itemViewModel = new ItemViewModel
-                    {
-                        Titulo = puntoModel.Nombre, 
-                        Punto = punto,
-                        Index = i++,
-                    };
-                model.Add(itemViewModel);
-                CreateNewRecargaPushpin(itemViewModel);
-            }
+            ViewModel.LoadPuntosRecarga(l.Select(x => new ItemViewModel
+                {
+                    Punto = new GeoCoordinate(x.Latitud, x.Longitud),
+                    Titulo = x.Nombre,
+                }));
 
-            if (!ViewModel.IsPuntosRecargaLoaded)
-            {
-                ViewModel.LoadPuntosRecarga(model);
-            }
-
-            var x = from ll in ViewModel.Pushpins
+            var pp = from ll in ViewModel.Pushpins
                     select ll.Location;
-            Mapa.SetView(LocationRect.CreateLocationRect(x));
-
-
-            RecargaLoading.Visibility = Visibility.Collapsed;
-            RecargaError.Visibility = Visibility.Collapsed;
-            FinishRequest();
-        }
-
-        private void FinishRequest()
-        {
-            _pendingRequests--;
-            if (_pendingRequests != 0) return;
+            Mapa.SetView(LocationRect.CreateLocationRect(pp));
 
             ResetUI();
         }
 
-        private void ResetUI()
+        private void CancelarRequest()
         {
-            ProgressBar.Hide();
+            if (_httpReq != null)
+                _httpReq.Abort();
+        }
+
+        void SetApplicationBarEnabled(bool isEnabled)
+        {
             var applicationBarIconButton = ApplicationBar.Buttons[0] as ApplicationBarIconButton;
             if (applicationBarIconButton != null)
-                applicationBarIconButton.IsEnabled = true;
+                applicationBarIconButton.IsEnabled = isEnabled;
         }
-        
-        private static void SetLocation(GeoCoordinate location)
-        {
-            // Center map to default location.
-            ViewModel.Center = location;
 
-            // Reset zoom default level.
-            ViewModel.Zoom = ViewModel.DefaultZoomLevel;
+        private void ResetUI()
+        {
+            Loading.Visibility = Visibility.Collapsed;
+            ConnectionError.Visibility = Visibility.Collapsed;
+            ProgressBar.Hide();
+            SetApplicationBarEnabled(true);
         }
 
         private void Pushpin_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
@@ -262,7 +146,7 @@ namespace GuiaTBAWP.Views.SUBE
             NavigationService.Navigate(new Uri("/Views/Acerca.xaml", UriKind.Relative));
         }
 
-        private void MiMapa_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        private void MiMapa_Tap(object sender, GestureEventArgs e)
         {
             //NavigationService.Navigate(new Uri("/Views/SUBE/Mapa.xaml", UriKind.Relative));
         }

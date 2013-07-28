4281daf47a63fb2155eefc489a5d3f1b220e26df
abanzas <abanzas@localhost>
2013-07-28 18:30:31 -0300

    estado de vuelos

---
+++ GuiaTBAWP/ViewModels/AirportStatusViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace GuiaTBAWP.ViewModels
+{
+    public class AirportStatusViewModel : INotifyPropertyChanged
+    {
+        public AirportStatusViewModel()
+        {
+            Vuelos = new ObservableCollection<AirportStatusItemViewModel>();
+        }
+
+        private string _actualizacion;
+        public string Actualizacion
+        {
+            get { return _actualizacion; }
+            set
+            {
+                _actualizacion = value;
+                NotifyPropertyChanged("Actualizacion");
+            }
+        }
+
+        private string _titulo;
+        public string Titulo
+        {
+            get { return _titulo; }
+            set
+            {
+                _titulo = value.ToLowerInvariant();
+                NotifyPropertyChanged("Titulo");
+            }
+        }
+
+        private string _aeroestacion;
+        public string Aeroestacion
+        {
+            get { return _aeroestacion; }
+            set
+            {
+                _aeroestacion = value.ToUpperInvariant();
+                NotifyPropertyChanged("Aeroestacion");
+            }
+        }
+
+        private ObservableCollection<AirportStatusItemViewModel> _vuelos;
+        public ObservableCollection<AirportStatusItemViewModel> Vuelos
+        {
+            get { return _vuelos; }
+            private set { 
+                _vuelos = value;
+                NotifyPropertyChanged("Vuelos");
+            }
+        }
+
+        public string NickName { get; set; }
+
+        public string Tipo { get; set; }
+
+        public void AddVuelo(AirportStatusItemViewModel linea)
+        {
+            Vuelos.Add(linea);
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ GuiaTBAWP/Views/Aviones/Estado.xaml.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.Serialization.Json;
+using System.Windows;
+using System.Windows.Navigation;
+using GuiaTBAWP.Extensions;
+using GuiaTBAWP.Helpers;
+using GuiaTBAWP.Models;
+using GuiaTBAWP.ViewModels;
+using Microsoft.Phone.Shell;
+
+namespace GuiaTBAWP.Views.Aviones
+{
+    public partial class Arribos
+    {
+        WebRequest _httpReq;
+
+        private static AirportStatusViewModel _viewModel = new AirportStatusViewModel();
+        public static AirportStatusViewModel ViewModel
+        {
+            get { return _viewModel ?? (_viewModel = new AirportStatusViewModel()); }
+        }
+
+        public Arribos()
+        {
+            InitializeComponent();
+            
+            DataContext = ViewModel;
+            Loaded += (sender, args) =>
+                {
+                    LoadData();
+                };
+            Unloaded += (sender, args) =>
+                {
+                    if(_httpReq != null)
+                        _httpReq.Abort();
+                };
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var titulo = NavigationContext.QueryString["titulo"];
+            var aeroestacion = NavigationContext.QueryString["aeroestacion"];
+            var nickname = NavigationContext.QueryString["nickname"];
+            var tipo = NavigationContext.QueryString["tipo"];
+
+            ViewModel.Titulo = titulo;
+            ViewModel.Aeroestacion = aeroestacion;
+            ViewModel.NickName = nickname;
+            ViewModel.Tipo = tipo;
+            ViewModel.Vuelos.Clear();
+
+            base.OnNavigatedTo(e);
+        }
+
+        public void LoadData()
+        {
+            ResetUI();
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {    
+                ConnectionError.Visibility = Visibility.Visible;
+                Deployment.Current.Dispatcher.BeginInvoke(() => MessageBox.Show("Ha habido un error intentando acceder a los nuevos datos o no hay conexiones de red disponibles.\nPor favor asegúrese de contar con acceso de red y vuelva a intentarlo."));
+                return;
+            }
+            
+            ProgressBar.Show(string.Format("Obteniendo {0}...", ViewModel.Titulo));
+            ViewModel.Actualizacion = "Actualizando...";
+            SetApplicationBarEnabled(false);
+            ViewModel.Vuelos.Clear();
+
+            var param = new Dictionary<string, object>
+                {
+                    { "t", ViewModel.NickName },
+                };
+
+            _httpReq = WebRequest.Create(string.Format("/aviones/{0}", ViewModel.Tipo).ToApiCallUri(param, alwaysRefresh: true));
+            _httpReq.Method = "GET";
+            _httpReq.BeginGetResponse(HTTPWebRequestCallBack, _httpReq);
+        }
+
+        private void ResetUI()
+        {
+            ConnectionError.Visibility = Visibility.Collapsed;
+            ProgressBar.Hide();
+            SetApplicationBarEnabled(true);
+        }
+
+        void SetApplicationBarEnabled(bool isEnabled)
+        {
+            var applicationBarIconButton = ApplicationBar.Buttons[0] as ApplicationBarIconButton;
+            if (applicationBarIconButton != null)
+                applicationBarIconButton.IsEnabled = isEnabled;
+        }
+
+        private void HTTPWebRequestCallBack(IAsyncResult result)
+        {
+            try
+            {
+                var httpRequest = (HttpWebRequest)result.AsyncState;
+                var response = httpRequest.EndGetResponse(result);
+                var stream = response.GetResponseStream();
+
+                var serializer = new DataContractJsonSerializer(typeof(AvionesTerminalStatusModel));
+                var o = (AvionesTerminalStatusModel)serializer.ReadObject(stream);
+
+                Dispatcher.BeginInvoke(new DelegateUpdateEstado(UpdateEstadoServicio), o);
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.BeginInvoke(() =>
+                    {
+                        ResetUI();
+#if DEBUG
+                        MessageBox.Show(ex.ToString());
+#endif
+                        MessageBox.Show("Ocurrió un error al obtener el estado del servicio. Verifique su conexión a internet.");
+                    });
+            }
+        }
+
+        delegate void DelegateUpdateEstado(AvionesTerminalStatusModel estado);
+        private void UpdateEstadoServicio(AvionesTerminalStatusModel model)
+        {
+            BindViewModel(model);
+
+            ResetUI();
+
+            Config.Set(model);
+        }
+
+        private void BindViewModel(AvionesTerminalStatusModel model)
+        {
+            ViewModel.Vuelos.Clear();
+            foreach (var itemViewModel in model.Arribos.Where(x => x.Hora > DateTime.UtcNow.AddHours(-5) || !"aterrizado".Equals(x.Estado.ToLowerInvariant())))
+            {
+                ViewModel.AddVuelo(new AirportStatusItemViewModel
+                {
+                    Estado = GetEstadoByStatusModel(itemViewModel),
+                    Nombre = itemViewModel.Nombre,
+                    Ciudad = itemViewModel.Origen.Sanitize(),
+                    Terminal = string.Format("terminal {0}", itemViewModel.Terminal),
+                    Horario = string.Format("{0} {1}", itemViewModel.Hora.ToString("dd/MM"), itemViewModel.Hora.ToString("HH:mm")),
+                    Aerolinea = itemViewModel.Linea,
+                });
+            }
+            foreach (var itemViewModel in model.Partidas.Where(x => x.Hora > DateTime.UtcNow.AddHours(-5) || !"despegado".Equals(x.Estado.ToLowerInvariant())))
+            {
+                ViewModel.AddVuelo(new AirportStatusItemViewModel
+                {
+                    Estado = GetEstadoByStatusModel(itemViewModel),
+                    Nombre = itemViewModel.Nombre,
+                    Ciudad = itemViewModel.Destino.Sanitize(),
+                    Terminal = string.Format("terminal {0}", itemViewModel.Terminal),
+                    Horario = string.Format("{0} {1}", itemViewModel.Hora.ToString("dd/MM"), itemViewModel.Hora.ToString("HH:mm")),
+                    Aerolinea = itemViewModel.Linea,
+                });
+            }
+
+            ViewModel.Actualizacion = string.Format("Actualizado hace {0}.", model.Actualizacion.ToUpdateDateTime());
+        }
+
+        private string GetEstadoByStatusModel(VueloArriboModel itemViewModel)
+        {
+            if (string.IsNullOrWhiteSpace(itemViewModel.Estado))
+            {
+                return "no informa";
+            }
+            if (itemViewModel.Estado.ToLowerInvariant().Contains("estima") && itemViewModel.Estima.HasValue)
+            {
+                return string.Format("estima ({0})", itemViewModel.Estima.Value.ToString("HH:mm"));
+            }
+
+            return itemViewModel.Estado.Sanitize().ToLowerInvariant();
+        }
+
+        private string GetEstadoByStatusModel(VueloPartidaModel itemViewModel)
+        {
+            if (string.IsNullOrWhiteSpace(itemViewModel.Estado))
+            {
+                return "no informa";
+            }
+            if (itemViewModel.Estado.ToLowerInvariant().Contains("estima") && itemViewModel.Estima.HasValue)
+            {
+                return string.Format("estima ({0})", itemViewModel.Estima.Value.ToString("HH:mm"));
+            }
+
+            return itemViewModel.Estado.Sanitize().ToLowerInvariant();
+        }
+
+        private void ButtonRefresh_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string Sanitize(this string source)
+        {
+            return HttpUtility.HtmlDecode(source);
+        }
+    }
+}

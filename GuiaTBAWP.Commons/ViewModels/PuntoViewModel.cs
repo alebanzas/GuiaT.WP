e684c85c6acfe44645010de624353d08fe0580de
abanzas <abanzas@localhost>
2013-10-28 00:14:30 -0300

    mapa de recorrido de colectivos

---
+++ GuiaTBAWP.Commons/ViewModels/PuntoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GuiaTBAWP.Commons.ViewModels
+{
+    public class PuntoViewModel
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+    }
+}
--- GuiaTBAWP/Views/Colectivos/Colectivos.xaml.cs
+++ GuiaTBAWP/Views/Colectivos/Colectivos.xaml.cs
@@ -1,4 +1,6 @@
-﻿using GuiaTBAWP.BusData;
+﻿using System;
+using System.Windows.Controls;
+using GuiaTBAWP.BusData;
 using Microsoft.Phone.Controls;
 
 namespace GuiaTBAWP.Views.Colectivos
@@ -12,6 +14,18 @@ namespace GuiaTBAWP.Views.Colectivos
             DataColectivos.SetData(busesIUrb, DataColectivos.LoadBusesIUrb());
             DataColectivos.SetData(busesProv, DataColectivos.LoadBusesProv());
             DataColectivos.SetData(busesMuni, DataColectivos.LoadBusesMuni());
+        }
+
+        private void Buses_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                var bus = ((Bus)e.AddedItems[0]).Title.Split(' ')[1];
+
+                Uri uri = new Uri(String.Format("/Views/Colectivos/Mapa.xaml?linea={0}", bus), UriKind.Relative);
+                NavigationService.Navigate(uri);
+            }
+            catch (Exception) {}
         }
     }
 }

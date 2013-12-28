1acaea24f8d90f256703a37674ee1df84d354e8a
ale <ale@localhost>
2013-12-28 11:06:58 -0300

    agrego converters

---
+++ GuiaTBAWP.Commons/Converters/SubteColorTituloConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Shapes;
+
+namespace GuiaTBAWP.Converters
+{
+    /// <summary>
+    /// Data binding converter for converting a line number to image source
+    /// </summary>
+    public class SubteColorTituloConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return "White";
+
+            try
+            {
+                var linestatus = value.ToString().ToLowerInvariant();
+
+                return linestatus.Contains("normal") ? "White" : "Yellow";
+            }
+            catch (Exception)
+            {
+                return "White";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}

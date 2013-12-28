3dbfd6de303ff529657ab0031cc68741fa333f89
Alejandro Banzas <alebanzas@outlook.com>
2013-12-28 12:43:04 -0300

    status trenes individual

--- GuiaTBAWP/Views/Trenes/Trenes.xaml.cs
+++ GuiaTBAWP/Views/Trenes/Trenes.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Windows;
-using GuiaTBAWP.ViewModels;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
 
 namespace GuiaTBAWP.Views.Trenes
 {
@@ -12,12 +10,7 @@ namespace GuiaTBAWP.Views.Trenes
         {
             InitializeComponent();
 
-            DataContext = ViewModel;
-            Loaded += MainPage_Loaded;
-
-            ViewModel.Lineas.Clear();
-            _dataService.EndRequest = EndRequest;
-            _dataService.StartRequest = StartRequest;
+            StatusChecker.Check("TrenesHome");
         }
 
         private void Button_Click_BelgranoNorte(object sender, RoutedEventArgs e)
@@ -54,58 +47,5 @@ namespace GuiaTBAWP.Views.Trenes
         {
             NavigationService.Navigate(new Uri("/Views/Trenes/Urquiza.xaml", UriKind.Relative));
         }
-        
-        #region Data
-
-        private static SubteStatusViewModel _viewModel = new SubteStatusViewModel();
-
-        private static TrenStatusService _dataService = new TrenStatusService();
-
-        public static SubteStatusViewModel ViewModel
-        {
-            get
-            {
-                // Delay creation of the view model until necessary
-                return _viewModel ?? (_viewModel = new SubteStatusViewModel());
-            }
-        }
-
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
-        {
-            if (!_dataService.DatosLoaded)
-                _dataService.LoadData();
-        }
-
-        private int EndRequest()
-        {
-            Deployment.Current.Dispatcher.BeginInvoke(() =>
-            {
-                var applicationBarIconButton = ApplicationBar.Buttons[0] as ApplicationBarIconButton;
-                if (applicationBarIconButton != null)
-                    applicationBarIconButton.IsEnabled = true;
-
-                ProgressBar.Hide();
-            });
-            return 0;
-        }
-
-        private int StartRequest()
-        {
-            Deployment.Current.Dispatcher.BeginInvoke(() =>
-            {
-                var applicationBarIconButton = ApplicationBar.Buttons[0] as ApplicationBarIconButton;
-                if (applicationBarIconButton != null)
-                    applicationBarIconButton.IsEnabled = false;
-
-                ProgressBar.Show("Actualizando estado del servicio...");
-            });
-            return 0;
-        }
-
-        private void ButtonGo_Click(object sender, EventArgs e)
-        {
-            _dataService.LoadData();
-        }
-        #endregion
     }
 }

11e37a17100dd111526627d3daca446cf5460e17
Alejandro Banzas <alebanzas@outlook.com>
2014-01-03 18:41:35 -0300

    arranco radio taxis

--- GuiaTBAWP/Views/Taxis/RadioTaxis.xaml.cs
+++ GuiaTBAWP/Views/Taxis/RadioTaxis.xaml.cs
@@ -1,14 +1,35 @@
-﻿using Microsoft.Phone.Controls;
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
 
 namespace GuiaTBAWP.Views.Taxis
 {
     public partial class RadioTaxis : PhoneApplicationPage
     {
+        private static RadioTaxisViewModel _viewModel = new RadioTaxisViewModel();
+        public static RadioTaxisViewModel ViewModel
+        {
+            get { return _viewModel ?? (_viewModel = new RadioTaxisViewModel()); }
+        }
+
         public RadioTaxis()
         {
             InitializeComponent();
 
+            Loaded += RadioTaxis_Loaded;
+
             StatusChecker.Check("Taxi.RadioTaxis");
         }
+
+        private void RadioTaxis_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+
+    public class RadioTaxisViewModel
+    {
+        public string Nombre { get; set; }
+
+        public string Telefono { get; set; }
     }
 }

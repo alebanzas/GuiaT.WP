c08c9a0c3d531bb63ac08614db4c55cedfaf017b
ale <ale@localhost>
2014-01-04 20:48:04 -0300

    view model taxis

---
+++ GuiaTBAWP.Commons/ViewModels/RadioTaxisItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GuiaTBAWP.Commons.ViewModels
+{
+    public class RadioTaxisItemViewModel
+    {
+        public string Nombre { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Url { get; set; }
+    }
+}
--- GuiaTBAWP/Views/Taxis/RadioTaxis.xaml.cs
+++ GuiaTBAWP/Views/Taxis/RadioTaxis.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using GuiaTBAWP.Commons.ViewModels;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
 
 namespace GuiaTBAWP.Views.Taxis
 {
@@ -15,6 +18,7 @@ namespace GuiaTBAWP.Views.Taxis
         {
             InitializeComponent();
 
+            DataContext = ViewModel;
             Loaded += RadioTaxis_Loaded;
 
             StatusChecker.Check("Taxi.RadioTaxis");
@@ -22,14 +26,33 @@ namespace GuiaTBAWP.Views.Taxis
 
         private void RadioTaxis_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Premium", Telefono = "5238-0000", Url = "http://www.taxipremium.com/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Premier", Telefono = "4858-0888", Url = "http://www.premieradiotaxi.com.ar/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Por Buenos Aires", Telefono = "4566-7777", Url = "http://www.bairesradiotaxi.com.ar/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Pídalo", Telefono = "4956-1200", Url = "http://www.radiotaxipidalo.com.ar/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Mi Radio Taxi", Telefono = "4931-1200", Url = "http://www.mitaxionline.com/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Tiempo", Telefono = "4854-3838", Url = "http://www.radiotaxitiemposrl.com.ar/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "City Tax", Telefono = "4585-5544", Url = "http://www.citytax.com.ar/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Porteño", Telefono = "4566-5777", Url = "http://www.radiotaxiportenio.com.ar/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Buen Viaje", Telefono = "5252-9999", Url = "http://www.radiotaxibuenviaje.com/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Aló", Telefono = "4855-5555", Url = "http://www.radiotaxisalo.com.ar/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Del Plata", Telefono = "4504-7776", Url = "http://www.delplataradiotaxi.com/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "24 Horas", Telefono = "4523-2222", Url = "http://www.radiotaxi24.com.ar/" });
+            ViewModel.Lista.Add(new RadioTaxisItemViewModel { Nombre = "Gold", Telefono = "4305-5050", Url = "http://www.radiotaxigoldsrl.com.ar/" });
         }
-    }
 
-    public class RadioTaxisViewModel
-    {
-        public string Nombre { get; set; }
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var lb = (ListBox) sender;
+            if (lb.SelectedItems != null)
+            {
+                var item = (RadioTaxisItemViewModel) lb.SelectedItems[0];
 
-        public string Telefono { get; set; }
+                var task = new PhoneCallTask();
+                task.DisplayName = item.Nombre;
+                task.PhoneNumber = item.Telefono;
+                task.Show();
+            }
+        }
     }
 }

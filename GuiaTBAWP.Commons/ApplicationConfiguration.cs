23ee13ccce80c28795927f65ee2e47bcfc3c197e
ale <ale@localhost>
2014-01-05 01:22:41 -0300

    radio taxi detalles

--- GuiaTBAWP.Commons/ApplicationConfiguration.cs
+++ GuiaTBAWP.Commons/ApplicationConfiguration.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Data.Linq;
 using System.Device.Location;
+using GuiaTBAWP.Commons.Models;
+using GuiaTBAWP.Commons.ViewModels;
 using GuiaTBAWP.Helpers;
 using GuiaTBAWP.Models;
 using System.Collections.Generic;
@@ -124,6 +127,21 @@ namespace GuiaTBAWP
             var trenService = new TrenDataService();
             trenService.UpdateStatus(trenesStatusModel);
 
+            RadioTaxiDC.Destroy();
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("16da2d29-e965-417e-8625-c3aa221b58bf"), Nombre = "Premium", Telefono = "5238-0000", Url = "http://www.taxipremium.com/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("8bae0725-5ab5-41a1-b861-60845b971255"), Nombre = "Premier", Telefono = "4858-0888", Url = "http://www.premieradiotaxi.com.ar/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("e1ac3a39-e1b1-4703-8b66-c72d819c3c8b"), Nombre = "Por Buenos Aires", Telefono = "4566-7777", Url = "http://www.bairesradiotaxi.com.ar/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("c787c3ee-3168-4045-8857-f07818ecd8a8"), Nombre = "Pídalo", Telefono = "4956-1200", Url = "http://www.radiotaxipidalo.com.ar/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("d6622428-9e5e-4edf-81a7-6f16b5ae20ed"), Nombre = "Mi Radio Taxi", Telefono = "4931-1200", Url = "http://www.mitaxionline.com/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("8b00205e-bcc4-41b5-a165-27c282728d46"), Nombre = "Tiempo", Telefono = "4854-3838", Url = "http://www.radiotaxitiemposrl.com.ar/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("568164f1-d1e7-449d-b74f-80210d8d0c35"), Nombre = "City Tax", Telefono = "4585-5544", Url = "http://www.citytax.com.ar/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("23f5bdae-ab7b-4bd7-80a5-60d598a0939f"), Nombre = "Porteño", Telefono = "4566-5777", Url = "http://www.radiotaxiportenio.com.ar/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("09e8d3a9-d17a-4ccb-a28b-6c3d8417d450"), Nombre = "Buen Viaje", Telefono = "5252-9999", Url = "http://www.radiotaxibuenviaje.com/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("b421a7dc-af5a-489b-bc6d-1bea96b7c901"), Nombre = "Aló", Telefono = "4855-5555", Url = "http://www.radiotaxisalo.com.ar/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("1965e1c2-db4a-4ea7-a8c1-3e1449b0f79d"), Nombre = "Del Plata", Telefono = "4504-7776", Url = "http://www.delplataradiotaxi.com/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("eee11015-d3aa-4f34-9ac4-223149a7d254"), Nombre = "24 Horas", Telefono = "4523-2222", Url = "http://www.radiotaxi24.com.ar/" });
+            RadioTaxiDC.Current.Lista.InsertOnSubmit(new RadioTaxiTable { Id = new Guid("2a6a1d88-5ec7-4ca2-a524-0d0e5d5f8add"), Nombre = "Gold", Telefono = "4305-5050", Url = "http://www.radiotaxigoldsrl.com.ar/" });
+            RadioTaxiDC.Current.SubmitChanges();
         }
 
         /// <value>Registered ID used to access map control and Bing maps service.</value>

e87f1d58a354c5f5f0ea2a5f491fe929411b3858
Alejandro Banzas <alebanzas@outlook.com>
2014-01-11 00:02:20 -0300

    home de ruta

---
+++ GuiaTBAWP/Views/Ruta/Home.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace GuiaTBAWP.Views.Ruta
+{
+    public partial class Home : PhoneApplicationPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+            
+            StatusChecker.Check("HomeRuta");
+        }
+    }
+}

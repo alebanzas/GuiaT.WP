b03c2d17ba88a7a425c44c694e0c8d2d23250963
Alejandro Banzas <alebanzas@outlook.com>
2014-01-29 21:49:36 -0300

    cambio de version

--- GuiaTBAWP/App.xaml.cs
+++ GuiaTBAWP/App.xaml.cs
@@ -11,7 +11,7 @@ namespace GuiaTBAWP
     public partial class App
     {
         private const string AppName = "GUIATBAWP";
-        private const string AppVersion = "1.6.2.0";
+        private const string AppVersion = "1.7.0.0";
 
         public static ApplicationConfiguration Configuration { get; set; }
         

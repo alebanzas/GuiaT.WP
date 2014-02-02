eb236a05c70ff6007b636b8fce1679c594b6bfa2
ale <ale@localhost>
2014-02-02 16:24:11 -0300

    init data

--- GuiaTBAWP.Commons/Models/TrackLine.cs
+++ GuiaTBAWP.Commons/Models/TrackLine.cs
@@ -6,6 +6,12 @@ namespace GuiaTBAWP.Commons.Models
 {
     public class TrackLine
     {
+        public TrackLine()
+        {
+            Trazado = new List<PuntoViewModel>();
+            Postas = new List<PuntoViewModel>();
+        }
+
         public string Nombre { get; set; }
         public IEnumerable<PuntoViewModel> Trazado { get; set; }
         public IEnumerable<PuntoViewModel> Postas { get; set; }

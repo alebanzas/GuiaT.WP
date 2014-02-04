0dd61aa9e4e591cc7a8e4a3440f4a0d50cbe27e6
Alejandro Banzas <alebanzas@outlook.com>
2014-02-04 09:38:15 -0300

    installation ID en reporte de error.

--- TrenesBAWP/GeneralError.xaml.cs
+++ TrenesBAWP/GeneralError.xaml.cs
@@ -68,7 +68,7 @@ namespace GuiaTBAWP
             RequestModel = new ErrorReportRequestModel
             {
                 Date = DateTime.UtcNow,
-                InstallationId = "", //TODO: installationID
+                InstallationId = App.Configuration.InstallationId.ToString(),
                 ErrorDetail = Exception.ToString(),
                 UserMessage = TxtComentario.Text,
             };

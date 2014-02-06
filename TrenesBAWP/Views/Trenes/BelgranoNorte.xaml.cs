04ec27dcec93258644312246b397bff66d308717
Alejandro Banzas <alebanzas@outlook.com>
2014-02-06 16:02:09 -0300

    trenes v1.7.1.0 + versiones ok

--- TrenesBAWP/Views/Trenes/BelgranoNorte.xaml.cs
+++ TrenesBAWP/Views/Trenes/BelgranoNorte.xaml.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
 using System.Linq;
 using System.Windows;
+using GuiaTBAWP;
+using GuiaTBAWP.Commons;
 using GuiaTBAWP.Commons.ViewModels;
 using GuiaTBAWP.Extensions;
 using GuiaTBAWP.Models;
 using Microsoft.Phone.Controls;
-using TrenesBAWP;
+using Microsoft.Phone.Shell;
+using TrenesBAWP.Services;
 
-namespace GuiaTBAWP.Views.Trenes
+namespace TrenesBAWP.Views.Trenes
 {
     public partial class BelgranoNorte : PhoneApplicationPage
     {
@@ -17,14 +20,16 @@ namespace GuiaTBAWP.Views.Trenes
 
             DataContext = ViewModel;
             Loaded += Page_Loaded;
+            Unloaded += (sender, args) => DataService.CancelRequest();
+
+            StatusChecker.Check("Trenes.BelgranoNorte");
+
+            ViewModel.Ramales.Clear();
+            DataService.EndRequest = EndRequest;
+            DataService.StartRequest = StartRequest;
         }
 
         private static TrenLineaItemViewModel _viewModel = new TrenLineaItemViewModel();
-
-        /// <summary>
-        /// A static ViewModel used by the views to bind against.
-        /// </summary>
-        /// <returns>The MainViewModel object.</returns>
         public static TrenLineaItemViewModel ViewModel
         {
             get
@@ -36,6 +41,13 @@ namespace GuiaTBAWP.Views.Trenes
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            if (!DataService.DatosLoaded)
+                DataService.LoadData();
+            FillViewModel();
+        }
+
+        private static void FillViewModel()
+        {
             var query = TrenesRamalEstadoDC.Current.ByLinea("belgrano norte");
             ViewModel.Ramales.Clear();
             foreach (var estadoTable in query.ToList())
@@ -43,6 +55,55 @@ namespace GuiaTBAWP.Views.Trenes
                 ViewModel.AddRamal(estadoTable.ConvertToTrenRamalItemViewModel());
             }
             ViewModel.Actualizacion = string.Format("Actualizado hace {0}.", App.Configuration.UltimaActualizacionTrenes.ToUpdateDateTime());
+        }
+
+        #region Data
+
+        private static readonly TrenStatusService DataService = new TrenStatusService();
+
+        private int EndRequest()
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                var applicationBarIconButton = ApplicationBar.Buttons[0] as ApplicationBarIconButton;
+                if (applicationBarIconButton != null)
+                    applicationBarIconButton.IsEnabled = true;
+
+                ProgressBar.Hide();
+                
+                FillViewModel();
+            });
+            return 0;
+        }
+
+        private int StartRequest()
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                var applicationBarIconButton = ApplicationBar.Buttons[0] as ApplicationBarIconButton;
+                if (applicationBarIconButton != null)
+                    applicationBarIconButton.IsEnabled = false;
+
+                ProgressBar.Show("Actualizando estado del servicio...");
+            });
+            return 0;
+        }
+
+        private void ButtonGo_Click(object sender, EventArgs e)
+        {
+            DataService.LoadData();
+        }
+        #endregion
+
+
+        private void VerEnMapa_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/Trenes/Mapa.xaml?linea=belgrano-norte", UriKind.Relative));
+        }
+
+        private void Pin_Click(object sender, EventArgs e)
+        {
+            TileManager.Set(new Uri("/Views/Trenes/BelgranoNorte.xaml", UriKind.Relative), "Belgrano Norte", new Uri("/Images/Home/trenes.png", UriKind.Relative));
         }
     }
 }
--- TrenesBAWP/Views/Trenes/Roca.xaml.cs
+++ TrenesBAWP/Views/Trenes/Roca.xaml.cs
@@ -1,12 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
+using GuiaTBAWP;
+using GuiaTBAWP.Commons;
 using GuiaTBAWP.Commons.ViewModels;
 using GuiaTBAWP.Extensions;
 using GuiaTBAWP.Models;
 using Microsoft.Phone.Controls;
-using TrenesBAWP;
+using Microsoft.Phone.Shell;
+using TrenesBAWP.Services;
 
-namespace GuiaTBAWP.Views.Trenes
+namespace TrenesBAWP.Views.Trenes
 {
     public partial class Roca : PhoneApplicationPage
     {
@@ -16,6 +20,13 @@ namespace GuiaTBAWP.Views.Trenes
         
             DataContext = ViewModel;
             Loaded += Page_Loaded;
+            Unloaded += (sender, args) => DataService.CancelRequest();
+
+            StatusChecker.Check("Trenes.Roca");
+
+            ViewModel.Ramales.Clear();
+            DataService.EndRequest = EndRequest;
+            DataService.StartRequest = StartRequest;
         }
 
         private static TrenLineaItemViewModel _viewModel = new TrenLineaItemViewModel();
@@ -35,13 +46,70 @@ namespace GuiaTBAWP.Views.Trenes
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            if (!DataService.DatosLoaded)
+                DataService.LoadData();
+            FillViewModel();
+        }
+
+        private static void FillViewModel()
+        {
             var query = TrenesRamalEstadoDC.Current.ByLinea("gral. roca");
             ViewModel.Ramales.Clear();
             foreach (var estadoTable in query.ToList())
             {
                 ViewModel.AddRamal(estadoTable.ConvertToTrenRamalItemViewModel());
             }
-            ViewModel.Actualizacion = string.Format("Actualizado hace {0}.", App.Configuration.UltimaActualizacionTrenes.ToUpdateDateTime());
+            ViewModel.Actualizacion = string.Format("Actualizado hace {0}.",
+                App.Configuration.UltimaActualizacionTrenes.ToUpdateDateTime());
+        }
+
+        #region Data
+
+        private static readonly TrenStatusService DataService = new TrenStatusService();
+
+        private int EndRequest()
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                var applicationBarIconButton = ApplicationBar.Buttons[0] as ApplicationBarIconButton;
+                if (applicationBarIconButton != null)
+                    applicationBarIconButton.IsEnabled = true;
+
+                ProgressBar.Hide();
+
+                FillViewModel();
+            });
+            return 0;
+        }
+
+        private int StartRequest()
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                var applicationBarIconButton = ApplicationBar.Buttons[0] as ApplicationBarIconButton;
+                if (applicationBarIconButton != null)
+                    applicationBarIconButton.IsEnabled = false;
+
+                ProgressBar.Show("Actualizando estado del servicio...");
+            });
+            return 0;
+        }
+
+        private void ButtonGo_Click(object sender, EventArgs e)
+        {
+            DataService.LoadData();
+        }
+        #endregion
+
+
+        private void VerEnMapa_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/Trenes/Mapa.xaml?linea=roca", UriKind.Relative));
+        }
+
+        private void Pin_Click(object sender, EventArgs e)
+        {
+            TileManager.Set(new Uri("/Views/Trenes/Roca.xaml", UriKind.Relative), "Roca", new Uri("/Images/Home/trenes.png", UriKind.Relative));
         }
     }
 }

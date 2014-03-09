32a2ab36261a4ac78df7a5fab5c54b024b67cb7f
ale <ale@localhost>
2014-03-09 15:37:16 -0300

    realtime tren sarmiento

---
+++ GuiaTBAWP/Services/TrenRealTimeStatusService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.Serialization.Json;
+using System.Windows;
+using System.Windows.Threading;
+using GuiaTBA.Common.Models;
+using GuiaTBAWP.Commons.Extensions;
+using GuiaTBAWP.Commons.Services;
+using GuiaTBAWP.Extensions;
+using GuiaTBAWP.Models;
+
+namespace GuiaTBAWP.Services
+{
+    public class TrenRealTimeStatusService
+    {
+        public TrenRealTimeStatusService(int seconds, string key)
+        {
+            SetRealTimeInterval(seconds, key);
+        }
+
+        private bool _datosLoaded;
+        public bool DatosLoaded
+        {
+            get { return _datosLoaded || App.Configuration.UltimaActualizacionTrenes > DateTime.UtcNow.AddMinutes(-5).ToLocalTime(); }
+            set { _datosLoaded = value; }
+        }
+
+        public WebRequest Request { get; set; }
+        public bool Canceled { get; private set; }
+        public Func<LiveTrenModel, LiveTrenModel> EndRequest { get; set; }
+        private DispatcherTimer _dt = new DispatcherTimer();
+
+        private void SetRealTimeInterval(int seconds, string key)
+        {
+            _dt = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(seconds),
+            };
+            _dt.Tick += (sender, args) => LoadData(key);
+            _dt.Start(); 
+            LoadData(key);
+        }
+        
+        private void LoadData(string key)
+        {
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {
+                Deployment.Current.Dispatcher.BeginInvoke(() => MessageBox.Show("Ha habido un error intentando acceder a los nuevos datos o no hay conexiones de red disponibles.\nPor favor asegúrese de contar con acceso de red y vuelva a intentarlo."));
+                return;
+            }
+
+            DatosLoaded = false;
+            Canceled = false;
+            if (Request != null) Request.Abort();
+            var client = new HttpClient();
+            var param = new Dictionary<string, object> {{"id", key}};
+            Request = client.Get("/api/livetren".ToApiCallUri(param, true));
+            Request.BeginGetResponse(HTTPWebRequestCallBack, Request);
+        }
+
+        private void HTTPWebRequestCallBack(IAsyncResult result)
+        {
+            try
+            {
+                var httpRequest = (HttpWebRequest)result.AsyncState;
+                var response = httpRequest.EndGetResponse(result);
+                var stream = response.GetResponseStream();
+
+                var serializer = new DataContractJsonSerializer(typeof(LiveTrenModel));
+                var o = (LiveTrenModel)serializer.ReadObject(stream);
+                DatosLoaded = true;
+                EndRequest(o);
+            }
+            catch (Exception ex)
+            {
+                if (!Canceled)
+                {
+                    ex.Log();
+                }
+            }
+        }
+
+        public void CancelRequest()
+        {
+            _dt.Stop();
+            Canceled = true;
+        }
+    }
+}
--- GuiaTBAWP/Views/Trenes/Sarmiento.xaml.cs
+++ GuiaTBAWP/Views/Trenes/Sarmiento.xaml.cs
@@ -8,6 +8,7 @@ using GuiaTBAWP.Extensions;
 using GuiaTBAWP.Models;
 using GuiaTBAWP.Services;
 using Microsoft.Phone.Shell;
+using GuiaTBA.Common.Models;
 
 namespace GuiaTBAWP.Views.Trenes
 {
@@ -19,7 +20,11 @@ namespace GuiaTBAWP.Views.Trenes
         
             DataContext = ViewModel;
             Loaded += Page_Loaded;
-            Unloaded += (sender, args) => DataService.CancelRequest();
+            Unloaded += (sender, args) =>
+            {
+                DataService.CancelRequest();
+                _dataRealTimeService.CancelRequest();
+            };
 
             StatusChecker.Check("Trenes.Sarmiento");
 
@@ -34,11 +39,106 @@ namespace GuiaTBAWP.Views.Trenes
             get { return _viewModel ?? (_viewModel = new TrenLineaItemViewModel()); }
         }
 
+        private static TrenRealTimeStatusService _dataRealTimeService;
+
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             if (!DataService.DatosLoaded)
                 DataService.LoadData();
             FillViewModel();
+            _dataRealTimeService = new TrenRealTimeStatusService(20, "sarmiento")
+            {
+                EndRequest = model =>
+                {
+                    FillRealTimeModel(model);
+                    return model;
+                }
+            };
+        }
+
+        private void FillRealTimeModel(LiveTrenModel model)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                TEV_Actualizacion.Text = model.Actualizacion.ToUpdateDateTime();
+                var estaciones = model.Estaciones.ToArray();
+                TEV_0_1.Text = estaciones[0].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[0].Ida1);
+                TEV_0_2.Text = estaciones[0].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[0].Ida2);
+
+
+                TEV_1_1.Text = estaciones[1].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[1].Ida1);
+                TEV_1_2.Text = estaciones[1].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[1].Ida2);
+                TEV_1_3.Text = estaciones[1].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[1].Vuelta1);
+                TEV_1_4.Text = estaciones[1].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[1].Vuelta2);
+
+                TEV_2_1.Text = estaciones[2].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[2].Ida1);
+                TEV_2_2.Text = estaciones[2].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[2].Ida2);
+                TEV_2_3.Text = estaciones[2].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[2].Vuelta1);
+                TEV_2_4.Text = estaciones[2].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[2].Vuelta2);
+
+                TEV_3_1.Text = estaciones[3].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[3].Ida1);
+                TEV_3_2.Text = estaciones[3].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[3].Ida2);
+                TEV_3_3.Text = estaciones[3].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[3].Vuelta1);
+                TEV_3_4.Text = estaciones[3].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[3].Vuelta2);
+
+                TEV_4_1.Text = estaciones[4].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[4].Ida1);
+                TEV_4_2.Text = estaciones[4].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[4].Ida2);
+                TEV_4_3.Text = estaciones[4].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[4].Vuelta1);
+                TEV_4_4.Text = estaciones[4].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[4].Vuelta2);
+
+                TEV_5_1.Text = estaciones[5].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[5].Ida1);
+                TEV_5_2.Text = estaciones[5].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[5].Ida2);
+                TEV_5_3.Text = estaciones[5].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[5].Vuelta1);
+                TEV_5_4.Text = estaciones[5].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[5].Vuelta2);
+
+                TEV_6_1.Text = estaciones[6].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[6].Ida1);
+                TEV_6_2.Text = estaciones[6].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[6].Ida2);
+                TEV_6_3.Text = estaciones[6].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[6].Vuelta1);
+                TEV_6_4.Text = estaciones[6].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[6].Vuelta2);
+
+                TEV_7_1.Text = estaciones[7].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[7].Ida1);
+                TEV_7_2.Text = estaciones[7].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[7].Ida2);
+                TEV_7_3.Text = estaciones[7].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[7].Vuelta1);
+                TEV_7_4.Text = estaciones[7].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[7].Vuelta2);
+
+                TEV_8_1.Text = estaciones[8].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[8].Ida1);
+                TEV_8_2.Text = estaciones[8].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[8].Ida2);
+                TEV_8_3.Text = estaciones[8].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[8].Vuelta1);
+                TEV_8_4.Text = estaciones[8].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[8].Vuelta2);
+
+                TEV_9_1.Text = estaciones[9].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[9].Ida1);
+                TEV_9_2.Text = estaciones[9].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[9].Ida2);
+                TEV_9_3.Text = estaciones[9].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[9].Vuelta1);
+                TEV_9_4.Text = estaciones[9].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[9].Vuelta2);
+
+                TEV_10_1.Text = estaciones[10].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[10].Ida1);
+                TEV_10_2.Text = estaciones[10].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[10].Ida2);
+                TEV_10_3.Text = estaciones[10].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[10].Vuelta1);
+                TEV_10_4.Text = estaciones[10].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[10].Vuelta2);
+
+                TEV_11_1.Text = estaciones[11].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[11].Ida1);
+                TEV_11_2.Text = estaciones[11].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[11].Ida2);
+                TEV_11_3.Text = estaciones[11].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[11].Vuelta1);
+                TEV_11_4.Text = estaciones[11].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[11].Vuelta2);
+
+                TEV_12_1.Text = estaciones[12].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[12].Ida1);
+                TEV_12_2.Text = estaciones[12].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[12].Ida2);
+                TEV_12_3.Text = estaciones[12].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[12].Vuelta1);
+                TEV_12_4.Text = estaciones[12].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[12].Vuelta2);
+
+                TEV_13_1.Text = estaciones[13].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[13].Ida1);
+                TEV_13_2.Text = estaciones[13].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[13].Ida2);
+                TEV_13_3.Text = estaciones[13].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[13].Vuelta1);
+                TEV_13_4.Text = estaciones[13].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[13].Vuelta2);
+
+                TEV_14_1.Text = estaciones[14].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[14].Ida1);
+                TEV_14_2.Text = estaciones[14].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[14].Ida2);
+                TEV_14_3.Text = estaciones[14].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[14].Vuelta1);
+                TEV_14_4.Text = estaciones[14].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[14].Vuelta2);
+
+                TEV_15_3.Text = estaciones[15].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[15].Vuelta1);
+                TEV_15_4.Text = estaciones[15].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[15].Vuelta2);
+            });
         }
 
         private static void FillViewModel()
@@ -52,7 +152,7 @@ namespace GuiaTBAWP.Views.Trenes
             ViewModel.Actualizacion = string.Format("Actualizado hace {0}.",
                 App.Configuration.UltimaActualizacionTrenes.ToUpdateDateTime());
         }
-
+        
         #region Data
 
         private static readonly TrenStatusService DataService = new TrenStatusService();

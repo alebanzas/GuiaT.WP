8c45403e29604512d97a3c923b0d4a8056579732
ale <ale@localhost>
2014-03-13 00:17:28 -0300

    mejora en la UI

--- GuiaTBAWP/Views/Trenes/Sarmiento.xaml.cs
+++ GuiaTBAWP/Views/Trenes/Sarmiento.xaml.cs
@@ -62,83 +62,83 @@ namespace GuiaTBAWP.Views.Trenes
             {
                 //TEV_Actualizacion.Text = model.Actualizacion.ToUpdateDateTime();
                 var estaciones = model.Estaciones.ToArray();
-                TEV_0_1.Text = estaciones[0].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[0].Ida1);
-                TEV_0_2.Text = estaciones[0].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[0].Ida2);
 
+                TEV_0_1.Text = GetTimeStringIda(estaciones[0]);
 
-                TEV_1_1.Text = estaciones[1].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[1].Ida1);
-                TEV_1_2.Text = estaciones[1].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[1].Ida2);
-                TEV_1_3.Text = estaciones[1].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[1].Vuelta1);
-                TEV_1_4.Text = estaciones[1].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[1].Vuelta2);
+                TEV_1_1.Text = GetTimeStringIda(estaciones[1]);
+                TEV_1_2.Text = GetTimeStringVuelta(estaciones[1]);
 
-                TEV_2_1.Text = estaciones[2].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[2].Ida1);
-                TEV_2_2.Text = estaciones[2].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[2].Ida2);
-                TEV_2_3.Text = estaciones[2].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[2].Vuelta1);
-                TEV_2_4.Text = estaciones[2].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[2].Vuelta2);
+                TEV_2_1.Text = GetTimeStringIda(estaciones[2]);
+                TEV_2_2.Text = GetTimeStringVuelta(estaciones[2]);
 
-                TEV_3_1.Text = estaciones[3].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[3].Ida1);
-                TEV_3_2.Text = estaciones[3].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[3].Ida2);
-                TEV_3_3.Text = estaciones[3].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[3].Vuelta1);
-                TEV_3_4.Text = estaciones[3].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[3].Vuelta2);
+                TEV_3_1.Text = GetTimeStringIda(estaciones[3]);
+                TEV_3_2.Text = GetTimeStringVuelta(estaciones[3]);
 
-                TEV_4_1.Text = estaciones[4].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[4].Ida1);
-                TEV_4_2.Text = estaciones[4].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[4].Ida2);
-                TEV_4_3.Text = estaciones[4].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[4].Vuelta1);
-                TEV_4_4.Text = estaciones[4].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[4].Vuelta2);
+                TEV_4_1.Text = GetTimeStringIda(estaciones[4]);
+                TEV_4_2.Text = GetTimeStringVuelta(estaciones[4]);
 
-                TEV_5_1.Text = estaciones[5].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[5].Ida1);
-                TEV_5_2.Text = estaciones[5].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[5].Ida2);
-                TEV_5_3.Text = estaciones[5].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[5].Vuelta1);
-                TEV_5_4.Text = estaciones[5].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[5].Vuelta2);
+                TEV_5_1.Text = GetTimeStringIda(estaciones[5]);
+                TEV_5_2.Text = GetTimeStringVuelta(estaciones[5]);
 
-                TEV_6_1.Text = estaciones[6].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[6].Ida1);
-                TEV_6_2.Text = estaciones[6].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[6].Ida2);
-                TEV_6_3.Text = estaciones[6].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[6].Vuelta1);
-                TEV_6_4.Text = estaciones[6].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[6].Vuelta2);
+                TEV_6_1.Text = GetTimeStringIda(estaciones[6]);
+                TEV_6_2.Text = GetTimeStringVuelta(estaciones[6]);
 
-                TEV_7_1.Text = estaciones[7].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[7].Ida1);
-                TEV_7_2.Text = estaciones[7].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[7].Ida2);
-                TEV_7_3.Text = estaciones[7].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[7].Vuelta1);
-                TEV_7_4.Text = estaciones[7].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[7].Vuelta2);
+                TEV_7_1.Text = GetTimeStringIda(estaciones[7]);
+                TEV_7_2.Text = GetTimeStringVuelta(estaciones[7]);
 
-                TEV_8_1.Text = estaciones[8].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[8].Ida1);
-                TEV_8_2.Text = estaciones[8].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[8].Ida2);
-                TEV_8_3.Text = estaciones[8].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[8].Vuelta1);
-                TEV_8_4.Text = estaciones[8].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[8].Vuelta2);
+                TEV_8_1.Text = GetTimeStringIda(estaciones[8]);
+                TEV_8_2.Text = GetTimeStringVuelta(estaciones[8]);
 
-                TEV_9_1.Text = estaciones[9].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[9].Ida1);
-                TEV_9_2.Text = estaciones[9].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[9].Ida2);
-                TEV_9_3.Text = estaciones[9].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[9].Vuelta1);
-                TEV_9_4.Text = estaciones[9].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[9].Vuelta2);
+                TEV_9_1.Text = GetTimeStringIda(estaciones[9]);
+                TEV_9_2.Text = GetTimeStringVuelta(estaciones[9]);
 
-                TEV_10_1.Text = estaciones[10].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[10].Ida1);
-                TEV_10_2.Text = estaciones[10].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[10].Ida2);
-                TEV_10_3.Text = estaciones[10].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[10].Vuelta1);
-                TEV_10_4.Text = estaciones[10].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[10].Vuelta2);
+                TEV_10_1.Text = GetTimeStringIda(estaciones[10]);
+                TEV_10_2.Text = GetTimeStringVuelta(estaciones[10]);
 
-                TEV_11_1.Text = estaciones[11].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[11].Ida1);
-                TEV_11_2.Text = estaciones[11].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[11].Ida2);
-                TEV_11_3.Text = estaciones[11].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[11].Vuelta1);
-                TEV_11_4.Text = estaciones[11].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[11].Vuelta2);
+                TEV_11_1.Text = GetTimeStringIda(estaciones[11]);
+                TEV_11_2.Text = GetTimeStringVuelta(estaciones[11]);
 
-                TEV_12_1.Text = estaciones[12].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[12].Ida1);
-                TEV_12_2.Text = estaciones[12].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[12].Ida2);
-                TEV_12_3.Text = estaciones[12].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[12].Vuelta1);
-                TEV_12_4.Text = estaciones[12].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[12].Vuelta2);
+                TEV_12_1.Text = GetTimeStringIda(estaciones[12]);
+                TEV_12_2.Text = GetTimeStringVuelta(estaciones[12]);
 
-                TEV_13_1.Text = estaciones[13].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[13].Ida1);
-                TEV_13_2.Text = estaciones[13].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[13].Ida2);
-                TEV_13_3.Text = estaciones[13].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[13].Vuelta1);
-                TEV_13_4.Text = estaciones[13].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[13].Vuelta2);
+                TEV_13_1.Text = GetTimeStringIda(estaciones[13]);
+                TEV_13_2.Text = GetTimeStringVuelta(estaciones[13]);
 
-                TEV_14_1.Text = estaciones[14].Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones[14].Ida1);
-                TEV_14_2.Text = estaciones[14].Ida2 == 0 ? "andén" : string.Format("{0} min", estaciones[14].Ida2);
-                TEV_14_3.Text = estaciones[14].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[14].Vuelta1);
-                TEV_14_4.Text = estaciones[14].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[14].Vuelta2);
+                TEV_14_1.Text = GetTimeStringIda(estaciones[14]);
+                TEV_14_2.Text = GetTimeStringVuelta(estaciones[14]);
 
-                TEV_15_3.Text = estaciones[15].Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones[15].Vuelta1);
-                TEV_15_4.Text = estaciones[15].Vuelta2 == 0 ? "andén" : string.Format("{0} min", estaciones[15].Vuelta2);
+                TEV_15_1.Text = GetTimeStringVuelta(estaciones[15]);
             });
+        }
+
+        private static string GetTimeStringIda(LiveTrenItemModel estaciones)
+        {
+            if (estaciones.Ida1 == -1 && estaciones.Ida2 == -1)
+            {
+                return "no hay servicios";
+            }
+
+            var i = "prox. a Tigre en ";
+            i += (estaciones.Ida1 == 0 ? "andén" : string.Format("{0} min", estaciones.Ida1));
+            if (estaciones.Ida2 == -1) return i;
+            if (estaciones.Ida2 != 0)
+                i += string.Format(" y en {0} min", estaciones.Ida2);
+            return i;
+        }
+
+        private static string GetTimeStringVuelta(LiveTrenItemModel estaciones)
+        {
+            if (estaciones.Vuelta1 == -1 && estaciones.Vuelta2 == -1)
+            {
+                return "no hay servicios";
+            }
+
+            var i = "prox. a Retiro en ";
+            i += (estaciones.Vuelta1 == 0 ? "andén" : string.Format("{0} min", estaciones.Vuelta1));
+            if (estaciones.Vuelta2 == -1) return i;
+            if (estaciones.Vuelta2 != 0)
+                i += string.Format(" y en {0} min", estaciones.Vuelta2);
+            return i;
         }
 
         private static void FillViewModel()

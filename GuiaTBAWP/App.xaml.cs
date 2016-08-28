3daa22a126b40efe15ecc651fbefc09a235c42b6
Alejandro Banzas <alebanzas@outlook.com>
2016-08-28 20:41:13 -0300

    nueva version 2.3.0.8

--- GuiaTBAWP/App.xaml.cs
+++ GuiaTBAWP/App.xaml.cs
@@ -17,7 +17,7 @@ namespace GuiaTBAWP
     public partial class App
     {
         private const string AppName = "GUIATBAWP";
-        private const string AppVersion = "2.2.3.8";
+        private const string AppVersion = "2.3.0.8";
 
         public static ApplicationConfiguration Configuration { get; set; }
 
